45551feb90c4556fff43a290ee9b500fc174a93a
diegoalonsoocampo <92986071+diegoalonsoocampo@users.noreply.github.com>
2021-12-10 13:45:04 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,29 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+/*	var moito = 9 ;
+    var raiz = Math.sqrt(moito); /* Math.sqrt es para hacer la raiz de un numero */
+/*  alert(raiz); */
+
+	var uno = 25.2541 ;
+    var raiz = Math.sqrt(uno); /* Math.sqrt es para hacer la raiz de un numero */
+    alert(raiz); 
+
+    var dos = 9.3547 ;
+    var cuadrado = Math.pow(dos , 2); /* el cuadrado de un numero */
+    alert(cuadrado); 
+
+    var tres =Math.PI ;
+    var PI = Math.round(tres); /* valor do número PI sen decimais */
+    alert(PI); 
+
+    var cuatro= (x= 10 , y = 12) ;
+    var funcion = Math.min(x, y); /* A función que responde coa menor das variables */
+    alert(funcion);
+
+    var cinco = (0 - 3) ;
+    var aleatorio = Math.random(cinco) ; /* Unha función que xenera un número aleatorio entre cero e tres */
+    alert(aleatorio);
