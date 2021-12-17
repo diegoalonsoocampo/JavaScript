daef28858d21bf420d2e9f90580d6ced8a1c4d3a
diegoalonsoocampo <92986071+diegoalonsoocampo@users.noreply.github.com>
2021-12-17 12:27:21 +0100

    Add files via upload

---
+++ tarefa5_ducias2ovos/escripito.js
@@ -0,0 +1,11 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+	var huevos;
+    huevos = prompt("Cuantas docenas de huevos lleva una tortilla?   ");
+    alert("Para hacer una tortillas usas " + huevos + " docena/as");
+
+     
